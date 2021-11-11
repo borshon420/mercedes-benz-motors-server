182be4f89c4319788ba6918e0e1cc7d189afce6a
borshon420 <borshonarafat420@gmail.com>
2021-11-12 02:55:51 +0600

    delete product for admin

--- index.js
+++ index.js
@@ -56,6 +56,15 @@ async function run(){
             res.json(result);
         });
 
+        // DELETE API
+        app.delete('/products/:id', async(req, res)=> {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productsCollection.deleteOne(query);
+            console.log('deleting result id', result)
+            res.json(result)
+        });
+
         
 
         //GET ORDER
